de6a9c07fc026275b963b1d9942551880f974574
unknown <spectrages@gmail.com>
2023-02-03 12:10:31 +0300

    Docker + webpack fix

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,12 +1,15 @@
 const { merge } = require('webpack-merge');
 const common = require('./webpack.common.js');
-//require("dotenv").config({ override: true });
+const dotenv = require("dotenv").config({ override: true });
+const devURL = dotenv.parsed.BASE_URL;
+const devPORT = dotenv.parsed.PORT;
 
 module.exports = merge(common, {
   mode: 'development',
   devtool: 'inline-source-map',
   devServer: {
     static: './dist',
-    //port: process.env.PORT
+    host: devURL || "0.0.0.0",
+    port: devPORT || 8080,
   },  
 });
