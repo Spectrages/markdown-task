4f0f03dae8c00e0ffc8e06a7f0cd2155b467198b
unknown <spectrages@gmail.com>
2023-02-06 09:52:26 +0300

    Fix anim

--- src/script/animation.js
+++ src/script/animation.js
@@ -1,17 +1,22 @@
-
-console.log("I worked!")
-const id = null;
-function myMove() {
-    let elem = document.getElementById("square");
-    let pos = 0;
-    clearInterval(id)
-    id = setInterval(frame, 5)
-    function frame() {
-        if (pos == 350) {
-            clearInterval(id);
-        } else {
-            pos++;
-            elem.style.left = pos + 'px';
-        }
+let direction = 'left';
+let i = 0;
+let square = document.querySelector(".square");
+let articleWidth = document.querySelector('.article').getBoundingClientRect().width;
+console.log('work')
+function move() {
+    if(i > (articleWidth - 70)) {
+      direction = 'right' 
+    }
+    else if (i < 0){
+      direction = 'left'
+    }
+    if(direction === 'left'){
+      square.style.left = i + 'px';
+      i++;
+    }
+    else if(direction === 'right'){
+      square.style.left = i + 'px';
+      i--;
     }
 }
+window.onload = setInterval(move, 25)
