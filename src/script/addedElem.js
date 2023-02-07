000d31e3f5772d8204df592b05ba39c9f1c5004f
unknown <spectrages@gmail.com>
2023-02-07 17:53:38 +0300

    Fix

--- src/script/addedElem.js
+++ src/script/addedElem.js
@@ -3,19 +3,26 @@ let result = document.getElementById('result');
 let list = document.getElementById('myList');
 
 let arr = [];
-for (var i = 1; i <= 1000000; i++) {
+for (var i = 1; i <= 100000; i++) {
     arr.push(i.toString());
 }
 
-let testArr = new Array(100000);
+let testArr = new Array(10);
 testArr.fill({'key':"Key", "value":"Value"})
-
 
 input.addEventListener('keyup', (event) => {
     let resultArray = []
     list.innerHTML = ''
     if (event.target.value !== '') {
-        resultArray = arr.filter(item => item.includes(event.target.value)).slice(0, 10);
+        let reg = new RegExp(`(${event.target.value})\w*`)
+
+        //regexp
+        //resultArray = arr.filter(item => item.match(reg)).slice(0, 10);
+
+        //standart
+        //resultArray = arr.filter(item => item.includes(event.target.value)).slice(0, 10);
+
+
         resultArray.forEach((item) => {
             let elem = document.createElement('li')
             elem.className = 'flowElem';
@@ -24,3 +31,18 @@ input.addEventListener('keyup', (event) => {
         })
     }
 })
+
+/*
+input.addEventListener('keyup', (event) => {
+    let resultArray = []
+    list.innerHTML = ''
+    if (event.target.value !== '') {
+        resultArray = testArr.filter(item => {
+            for(const [key, value] of Object.entries(item)) {
+                console.log(key, value)
+            }
+        })
+    }
+})
+
+*/
